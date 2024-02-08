4c29717c7b54da34f81080b15ba302cd9364c8e3
Honzoraptor <j.ackermann@email.cz>
2024-02-08 13:35:31 +0100

    Added a time and date to each message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -14,8 +14,11 @@ function Message({ sentBy, timestamp, text, pfpUrl, sentByEmail }) {
   return (
     <div id={timestamp} className={user ? `message message-${sentByEmail === user.email ? "me" : "someone"}` : `message`}>
       <div className="message-top">
-        <img className="no-select" src={pfpUrl} />
-        <p className="message-username">{sentBy}</p>
+        <div className="message-user-info">
+          <img className="no-select" src={pfpUrl} />
+          <p className="message-username">{sentBy}</p>
+        </div>
+        <p className="message-date">{`${new Date(timestamp).toLocaleDateString("de-DE")} ${new Date(timestamp).getHours()}:${new Date(timestamp).getMinutes()}`}</p>
       </div>
       <p className="message-text">{text}</p>
     </div>
