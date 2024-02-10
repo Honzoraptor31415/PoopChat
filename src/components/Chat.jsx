50b0516fe14ecd1c7a93479928819f1be0984c45
Honzoraptor <j.ackermann@email.cz>
2024-02-10 11:21:47 +0100

    Added a pooping functionality BUT
    So far it could be little buggy, I'll be focusing on fixing those bugs later.

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -9,6 +9,8 @@ function Chat() {
   const [messages, setMessages] = useState()
   const [msgError, setMsgError] = useState()
   const [meFadeAway, setMeFadeAway] = useState(false)
+  const [poopStatus, setPoopStatus] = useState()
+  const [randomPlaceholder, setRandomPlaceholder] = useState(inputPlaceholders[Math.floor(Math.random() * inputPlaceholders.length)])
   const oldestMessageTime = 172_800_000
 
   async function send() {
@@ -43,6 +45,28 @@ function Chat() {
   async function getUser() {
     const { data: { user } } = await supabase.auth.getUser()
     setUser(user)
+    createUserInDB(user)
+  }
+
+  const hanldePooped = (payload) => {
+    console.log(payload)
+    if (payload.new.userEmail === user.email) {
+      setPoopStatus(payload.new)
+    }
+  }
+
+  supabase.channel('users').on('postgres_changes', { event: 'UPDATE', schema: 'public', table: 'users' }, hanldePooped).subscribe()
+
+  async function createUserInDB(u) {
+    const { data, error } = await supabase.from("users").select("*").eq("userEmail", u.email)
+    if (data.length > 0) {
+      // user in database already exists
+      console.log("User already exists in the DB")
+      setPoopStatus(data[0])
+    } else {
+      // user doesn't exist in the database
+      const { error } = await supabase.from("users").insert({ lastGotPooped: null, lastPoopedSomeone: null, userEmail: u.email })
+    }
   }
 
   const handleInserts = (payload) => {
@@ -65,46 +89,65 @@ function Chat() {
 
   return (
     <>
-      {msgError ? <p className="msg-error" style={meFadeAway ? { right: "-100vw" } : {}} >{msgError}</p> : ""}
-      <div className="container">
-        <div id="chat" className="chat">
-          <div className="messages-wrp">
-            {messages ? (
+      {poopStatus ? (
+        <>
+          {poopStatus.lastGotPooped > new Date().getTime() - 43_200_000 ? (
+            <>
+              <h1>You got pooped</h1>
+            </>
+          ) : (
+            <>
               <>
-                {messages.length > 0 ? (
-                  <>
-                    {messages.map(value => {
-                      return (
-                        <Message key={value.id} text={value.text} sentBy={value.sentBy} timestamp={value.timestamp} pfpUrl={value.pfpUrl} sentByEmail={value.sentByEmail} />
-                      )
-                    })}
-                  </>
-                ) : (
-                  <div className="no-messages">
-                    <p className="no-messages-text no-select">No messages were sent in the past 48 hours.</p>
+                {msgError ? <p className="msg-error" style={meFadeAway ? { right: "-100vw" } : {}} >{msgError}</p> : ""}
+                <div className="container">
+                  <div id="chat" className="chat">
+                    <div className="messages-wrp">
+                      {messages ? (
+                        <>
+                          {messages.length > 0 ? (
+                            <>
+                              {poopStatus ? (
+                                <>
+                                  {messages.map(value => {
+                                    return (
+                                      <Message key={value.id} text={value.text} sentBy={value.sentBy} timestamp={value.timestamp} pfpUrl={value.pfpUrl} sentByEmail={value.sentByEmail} userLastPooped={poopStatus} />
+                                    )
+                                  })}
+                                </>
+                              ) : ""}
+                            </>
+                          ) : (
+                            <div className="no-messages">
+                              <p className="no-messages-text no-select">No messages were sent in the past 48 hours.</p>
+                            </div>
+                          )}
+                        </>
+                      ) : ""}
+                    </div>
+                    <div className="about-link-wrp">
+                      <a href="/about" className="about-link">About PoopChat <span className="arrow">→</span></a>
+                    </div>
+                    <form onSubmit={(e) => {
+                      e.preventDefault()
+                      send()
+                      setRandomPlaceholder(inputPlaceholders[Math.floor(Math.random() * inputPlaceholders.length)])
+                      setMessage("")
+                    }} className="chat-ctrls">
+                      <div className={`input-wrp ${checkEmptyMessage(message) ? "input-wrp-cant-send" : "input-wrp-can-send"}`}>
+                        <div className="input-pfp-btn">
+                          <img className="input-pfp-img no-select" src={user ? user.user_metadata.avatar_url : ""} />
+                        </div>
+                        <input id="message-input" placeholder={randomPlaceholder} type="text" onChange={(e) => { setMessage(e.target.value) }} value={message} />
+                      </div>
+                      {checkEmptyMessage(message) ? "" : <input type="submit" value={"Submit"} />}
+                    </form>
                   </div>
-                )}
+                </div>
               </>
-            ) : ""}
-          </div>
-          <div className="about-link-wrp">
-            <a href="/about" className="about-link">About PoopChat <span className="arrow">→</span></a>
-          </div>
-          <form onSubmit={(e) => {
-            e.preventDefault()
-            send()
-            setMessage("")
-          }} className="chat-ctrls">
-            <div className={`input-wrp ${checkEmptyMessage(message) ? "input-wrp-cant-send" : "input-wrp-can-send"}`}>
-              <div className="input-pfp-btn">
-                <img className="input-pfp-img no-select" src={user ? user.user_metadata.avatar_url : ""} />
-              </div>
-              <input id="message-input" placeholder={inputPlaceholders[Math.floor(Math.random() * inputPlaceholders.length)]} type="text" onChange={(e) => { setMessage(e.target.value) }} value={message} />
-            </div>
-            {checkEmptyMessage(message) ? "" : <input type="submit" value={"Submit"} />}
-          </form>
-        </div>
-      </div>
+            </>
+          )}
+        </>
+      ) : ""}
     </>
   )
 }
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,27 +1,40 @@
 import { useState, useEffect } from "react"
 import { supabase } from "../supabaseClient"
 
-function Message({ sentBy, timestamp, text, pfpUrl, sentByEmail }) {
+function Message({ sentBy, timestamp, text, pfpUrl, sentByEmail, userLastPooped }) {
   const [user, setUser] = useState()
   async function getUser() {
     const { data: { user } } = await supabase.auth.getUser()
     setUser(user)
   }
+
+  async function poop() {
+    const { error } = await supabase.from("users").update({ lastGotPooped: new Date().getTime() }).eq("userEmail", sentByEmail)
+  }
+
   useEffect(() => {
     getUser()
-    document.getElementById(timestamp).scrollIntoView()
+    document.getElementById(`${timestamp}-bottom`).scrollIntoView()
   }, [])
   return (
-    <div id={timestamp} className={user ? `message message-${sentByEmail === user.email ? "me" : "someone"}` : `message`}>
-      <div className="message-top">
-        <div className="message-user-info">
-          <img className="no-select" src={pfpUrl} />
-          <p className="message-username">{sentBy}</p>
+    <>
+      <div id={timestamp} className={user ? `message message-${sentByEmail === user.email ? "me" : "someone"}` : `message`}>
+        <div className="message-top">
+          <div className="message-user-info">
+            <img className="no-select" src={pfpUrl} />
+            <p className="message-username">{sentBy}</p>
+          </div>
+          <p className="message-date">{`${new Date(timestamp).toLocaleDateString("de-DE")} ${new Date(timestamp).getHours()}:${new Date(timestamp).getMinutes()}`}</p>
+          {user ? (
+            <>
+              {sentByEmail === user.email ? "" : <button onClick={poop} className="poop-btn">{sentBy.length <= 9 ? `Poop ${sentBy}` : "Poop this user"}</button>}
+            </>
+          ) : ""}
         </div>
-        <p className="message-date">{`${new Date(timestamp).toLocaleDateString("de-DE")} ${new Date(timestamp).getHours()}:${new Date(timestamp).getMinutes()}`}</p>
+        <p className="message-text">{text}</p>
       </div>
-      <p className="message-text">{text}</p>
-    </div>
+      <div id={`${timestamp}-bottom`} style={{ visibility: "hidden" }}></div>
+    </>
   )
 }
 
