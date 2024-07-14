9092614cb4dbd80461ee2caeb2aa0b312ad451a3
Honzoraptor <151644171+Honzoraptor31415@users.noreply.github.com>
2024-07-14 22:56:51 +0200

    Fixed spelling mistake
    Oh jeez, how did I not notice earlier?

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -10,7 +10,7 @@ function About() {
   return (
     <div className="about-section">
       <h2>About PoopChat</h2>
-      <p className="about-text">PoopChat is a chat app made by <a href="https://github.com/Honzoraptor31415">Honzoraptor</a> in about three days (the first version) when he (me xD) was border. So far it's just the basic chat app, but in the future, there will be a pooping functionality, which a user can use to make someone unable to send messages for a specific time period, or maybe take controll over the specific user. </p>
+      <p className="about-text">PoopChat is a chat app made by <a href="https://github.com/Honzoraptor31415">Honzoraptor</a> in about three days (the first version) when he (me xD) was bored. So far it's just the basic chat app, but in the future, there will be a pooping functionality, which a user can use to make someone unable to send messages for a specific time period, or maybe take controll over the specific user. </p>
       <h2 id='about-pooping'>About the poop functionality</h2>
       <p className="about-text">You can use the <button className="poop-btn">Poop button</button> to poop someone - make them unable to send messages or view the chat for <b>2 hours!</b> However be careful, because the moment you send a message, anyone who's looking at the chat can poop you immediately.</p>
     </div>
